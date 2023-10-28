3b8b65a3e80242eec14125ce61e9213614d477ce
Shux <shu_x@outlook.com>
2023-10-28 09:27:43 +0800

    with custom dictionary for rides

--- notebooks/text_preprocessing.py
+++ notebooks/text_preprocessing.py
@@ -2,29 +2,92 @@
 Text preprocessing script
 
 Steps:
-1. Expand contractions
-2. Convert numbers to words
-3. Convert to lowercase
-4. Remove punctuations and special characters
-5. Stemming/Lemmatizing
+1. Convert to lowercase
+2. Replace ride names with 'ride'
+3. Expand contractions
+4. Lemmatising
+5. Split into tokens
+6. Convert numbers to words
+7. Keep tokens with letters only
+8. Remove stopwords
 '''
 
 import contractions
 from num2words import num2words
-# from nltk.corpus import PlaintextCorpusReader, stopwords
-# from nltk.stem import WordNetLemmatizer, PorterStemmer
+import nltk
+from nltk.corpus import PlaintextCorpusReader, stopwords
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer, PorterStemmer
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.snowball import SnowballStemmer
 import re
+from collections import Counter
+
+# Define list of rides
+ride_name = [
+    'tarzan tree house', 'slinky dog spin', 'iron man', 'hyperspace', 'hyper space', 'spaceship',
+    'small world', 'star wars', 'dumbo', 'lion king', 'boat ride', 'mystic mansion',
+    'honey hunt', 'grizzley gulch', 'carousel', 'teacup', 'tea cup', 'thunder mountain', 'buzz',
+    'monsters', 'indiana', 'haunted', 'pirates', 'star tours', "peter pan's flight", 'singing bear',
+    'mickey', 'space mountain', 'western river railroad', 'pirates of the caribbean', 'jungle' , 'cruise',
+    'tiki', 'riverboat', 'tom sawyer', 'splash mountain', 'beaver brothers', "alice's tea party", 'castle carrousel',
+    "snow white's adventure", 'dumbo the flying elephant', 'enchanted tale of beauty and the beast', "pinocchio's daring journey",
+    'haunted mansion', 'go coaster', "roger rabbit's car toon spin", 'astro blasters', 'happy ride with baymax', 'ride & go seek',
+    'animation academy', 'ant-man and the wasp', 'grizzly mountain', 'geyser gulch', 'hyperspace mountain', 'iron man experience',
+    'mad hatter', 'mystic manor', 'orbiton', 'rc racer', 'roaring rapids', "rex's racer", 'mine train', 'crystal grotto', 'jet packs',
+    'tron', 'lightcycle', 'space mountain', 'roller coaster', 'grizzly gulch']
+
+lemmatizer = WordNetLemmatizer()
+
+# Part-of-Speech Tagging
+def nltk_pos_tagger(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:          
+        return None
+    
+def lemmatize_sentence(sentence):
+    
+    nltk_tagged = nltk.pos_tag(nltk.word_tokenize(sentence))  
+    wordnet_tagged = map(lambda x: (x[0], nltk_pos_tagger(x[1])), nltk_tagged)
+    lemmatized_sentence = []
+    
+    for word, tag in wordnet_tagged:
+        if tag is None:
+            lemmatized_sentence.append(word)
+        else:        
+            lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
+    return " ".join(lemmatized_sentence)
 
 
-def text_preprocessing(text, stemmer = None, lemmatizer = None, stopword_list = None):
+def text_preprocessing(text, sub_ride = False, lemmatize = False, stem = False, stopword_list = None):
 
+    # Make lowercase
+    output = text.lower()
+    
+    # Replace ride name with 'ride'
+    if sub_ride == True:
+        for name in ride_name:
+            pattern = re.compile(rf'\b{re.escape(name)}\b', flags=re.IGNORECASE)
+            output = pattern.sub('ride', output)
+                
     # Expand contractions
-    output = contractions.fix(text)
+    output = contractions.fix(output)
+    
+    # Lemmatising
+    if lemmatize == True:
+        output = lemmatize_sentence(output)
 
     # Split into words
     output = word_tokenize(output)
     # output = output.split()
+    
     for i in range(len(output)):
 
         # Convert to number
@@ -34,9 +97,6 @@ def text_preprocessing(text, stemmer = None, lemmatizer = None, stopword_list = 
             except:
                 pass
 
-        # Make lowercase
-        output[i] = output[i].lower()
-
     # # Remove punctuation
     # pattern = r"[^\w\s]"
     # output = [re.sub(pattern, '', word) for word in output]
@@ -44,18 +104,15 @@ def text_preprocessing(text, stemmer = None, lemmatizer = None, stopword_list = 
     # Keep tokens with letters only
     pattern = r"^[a-z]+$"
     output = [word for word in output if re.search(pattern, word)]
-
+    
     # Define stopword list
     # stopword_list = stopwords.words('english')
     if stopword_list != None:
         output = [word for word in output if word not in stopword_list]
 
     # Stemming
-    if stemmer != None:
+    if stem == True:
+        stemmer = SnowballStemmer(language='english')
         output = [stemmer.stem(word) for word in output]
-
-     # Lemmatizing
-    if lemmatizer != None:
-        output = [lemmatizer.lemmatize(word) for word in output]
 
     return output
